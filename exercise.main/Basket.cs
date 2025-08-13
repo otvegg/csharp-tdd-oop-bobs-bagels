c3a0bef545c4bd0f7c7887e2096764a3e9d81a30
Oyvind Timian Dokk Husveg <oyvind.timian.dokk.husveg@no.experis.com>
2025-08-13 21:51:31 +0200

    Making the receipt better

--- exercise.main/Basket.cs
+++ exercise.main/Basket.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Emit;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -115,6 +116,75 @@ namespace exercise.main
         {
             string ReceiptSeperator = "\n------------------------------\n";
             string Receipt = $"    ~~~ Bob's Bagels ~~~\n\n     {DateTime.Now}\n{ReceiptSeperator}\n";
+
+            Dictionary<string, int> SKUs = products.Values.GroupBy(product => product.GetSKU()).ToDictionary(group => group.Key, group => group.Count());
+
+            Dictionary<string, int> bagelSKUs = SKUs.Where(sku => sku.Key.Contains("BGL")).ToDictionary();
+            int COFBs = products.Values.Where(product => product.GetSKU().Contains("COFB")).Count();
+            
+
+            // check how many bagels that fit into the 6/12 discount. If any bagels left, check if any coffees
+            decimal BasketValue = 0;
+            foreach (var sku in bagelSKUs)
+            {
+                decimal n_12s = 0;
+                decimal n_6s = 0;
+                decimal noDiscount = 0;
+                int bagelCOFBCombo = 0;
+                if (sku.Value >= 12)
+                {
+                    n_12s = Math.Floor(sku.Value / 12m);
+                    decimal remainder = sku.Value % 12;
+                    n_6s = remainder >= 6m ? 1 : 0;
+                    noDiscount = remainder % 6;
+                }
+                else if (sku.Value >= 6)
+                {
+                    n_6s++;
+                    noDiscount = sku.Value % 6;
+                }
+                else noDiscount = sku.Value;
+
+                //iterate number of combos of coffee+bagel
+                for (; bagelCOFBCombo < Math.Min(COFBs, noDiscount); bagelCOFBCombo++) ;
+
+                COFBs -= bagelCOFBCombo;
+
+
+                decimal discount = GetProduct(sku.Key).GetPrice() * sku.Value -  (n_6s * 2.49m + n_12s * 3.99m);
+                Receipt += ""
+                BasketValue += n_12s * 3.99m + n_6s * 2.49m + noDiscount * GetProduct(sku.Key).GetPrice();
+
+                foreach (var product in products.Values.Where(product => (product.GetSKU() == sku.Key)))
+                {
+                    BasketValue += (product as Bagel).GetFillingPrice();
+                    List<Filling> fillings = (product as Bagel).GetFillings();
+                    if (fillings.Count > 0)
+                    {
+                        foreach (var filling in fillings) { 
+                            Receipt += $"     * {filling.GetVariant()} -  £{filling.GetPrice()}\n"; 
+                        }
+
+                    }
+                }
+                
+            }
+
+            foreach (var product in products.Values)
+            {
+                // only add fillings
+                string sku = product.GetSKU();
+                if (sku.Contains("BGL")) BasketValue += (product as Bagel).GetFillingPrice();
+                else if (sku.Equals("COFB") && COFBs > 0)
+                {
+                    COFBs -= 1;
+                    BasketValue += product.GetPrice();
+                }
+                else BasketValue += product.GetPrice();
+
+            }
+
+            // Original
             foreach (var product in products.Values)
             {
                 Receipt += $"    {product.GetName()}, {product.GetVariant()} - £{product.GetPrice()}\n";
